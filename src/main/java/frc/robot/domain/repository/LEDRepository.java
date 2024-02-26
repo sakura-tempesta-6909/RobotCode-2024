fd44db91b9f33a903327fb13478e68f16096fbdf
miyu <happysweets2026@gmail.com>
2024-02-26 19:58:44 +0900

    fixed Repository

--- src/main/java/frc/robot/domain/repository/LEDRepository.java
+++ src/main/java/frc/robot/domain/repository/LEDRepository.java
@@ -4,16 +4,25 @@ package frc.robot.domain.repository;
 public interface LEDRepository {
     public enum LEDFlashes {
         //LEDの点滅のパターン
-        patterntheLEDFlashes;
+        
+        //ずっとついている
+        AlwaysOn,
+
+        //１秒ごとに点滅を繰り返す
+        BlinkingPerSec,
+
+        //ずっと消えている
+        AlwaysOff;
+        
     }
     /**
      * LEDを変えたい色に変える
      *
-     * @param LEDREGSequence    LEDの色の配列
+     * @param LEDRGBSequence    LEDの色の配列
      * @param patterntheLED  LEDの点滅の仕方
      * 
      * この状態の場合はこの色という情報はchangeModelでやる
      */
-    void changeLight(int[] LEDREGSequence, LEDFlashes pattern);
+    void changeLight(int[] LEDRGBSequence, LEDFlashes pattern);
 
 }
--- src/main/java/frc/robot/domain/repository/LinkRepository.java
+++ src/main/java/frc/robot/domain/repository/LinkRepository.java
@@ -4,7 +4,7 @@ public interface LinkRepository {
     /**
      * リンクを曲げることによってシューターを傾ける
      *
-     * @param TargetShooterAngle 目標のlinkAngle シューターの角度を上げる方向を正とする
+     * @param TargetShooterAngle 目標のlinkAngle[deg] シューターの角度を上げる方向を正とする
      * 
      */
     void MoveShooterToSpecifiedAngle(double TargetShooterAngle);
