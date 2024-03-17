316aae4a5c76ffd7d7ec0ddb5f38ba1818d93228
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2024-03-17 17:08:21 +0900

    port番号の変更

--- src/main/java/frc/robot/components/shooter/ShooterConst.java
+++ src/main/java/frc/robot/components/shooter/ShooterConst.java
@@ -2,9 +2,9 @@ package frc.robot.components.shooter;
 
 public class ShooterConst {
     public static final class Ports {
-        public static final int NoteLowerShooter = 1;
-        public static final int NotePusher = 2;
-        public static final int NoteUpperShooter = 3;
+        public static final int NoteLowerShooter = 13;
+        public static final int NotePusher = 14;
+        public static final int NoteUpperShooter = 15;
         public static final int NoteDirectionSensor = 9;
     }
 }
