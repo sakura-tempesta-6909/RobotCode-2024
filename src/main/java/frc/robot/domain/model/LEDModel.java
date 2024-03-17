a0c7469ba795bc48813822983c53fb68f79e8066
miyu <happysweets2026@gmail.com>
2024-03-17 13:22:34 +0900

    commentを綺麗にします

--- src/main/java/frc/robot/domain/model/LEDModel.java
+++ src/main/java/frc/robot/domain/model/LEDModel.java
@@ -4,11 +4,9 @@ import frc.robot.domain.repository.LEDRepository.LEDFlashes;
 
 public class LEDModel {
 
-    /**
-     * sequenceはLEDの点滅の色を表しているintの配列
-     * patternはLEDの点滅の仕方を表すenum型です
-     */
+    /** LEDの点滅の色を表しているintの配列 */
     public static int[] sequence;
+    /** patternはLEDの点滅の仕方を表すenum型です */
     public static LEDFlashes pattern;
 
     public static void reset() {
