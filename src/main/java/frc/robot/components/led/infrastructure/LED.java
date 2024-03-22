aa956b0170360d86f0481b31978b526d930d5be4
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2024-03-22 18:06:06 +0900

    deketa

--- src/main/java/frc/robot/components/led/infrastructure/LED.java
+++ src/main/java/frc/robot/components/led/infrastructure/LED.java
@@ -3,6 +3,7 @@ package frc.robot.components.led.infrastructure;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.components.led.LEDConst;
 import frc.robot.components.led.LEDParameter;
 import frc.robot.domain.measure.LEDMeasuredState;
@@ -14,6 +15,7 @@ public class LED implements LEDRepository {
     final AddressableLEDBuffer ledBuffer;
     final Timer timer;
     int rainbowFirstPixelHue;
+
     public LED() {
         led = new AddressableLED(LEDConst.Ports.LED);
         ledBuffer = new AddressableLEDBuffer(LEDConst.Ports.LEDBuffer);
@@ -21,30 +23,36 @@ public class LED implements LEDRepository {
         led.setData(ledBuffer);
         led.start();
         timer = new Timer();
+        timer.start();
         LEDParameter.ConstInit();
     }
+
     @Override
     public void readSensors() {
-
+        SmartDashboard.putNumber("Timer", timer.get());
     }
+
     @Override
     public void changeLight() {
         for (var i = 0; i < ledBuffer.getLength(); i++) {
-            ledBuffer.setRGB(i, 255, 125, 255);
+            ledBuffer.setRGB(i, 255, 0, 255);
         }
         led.setData(ledBuffer);
     }
+
     @Override
     public void flashLight() {
         for (var i = 0; i < ledBuffer.getLength(); i++) {
-            if (timer.get() < 1) {
-                ledBuffer.setRGB(i, 255, 120, 60);
-            } else if (timer.get() < 2) {
-                led.stop();
+            if (timer.get() < 0.1) {
+                ledBuffer.setRGB(i, 255, 50, 0);
+            } else if (timer.get() < 0.2) {
+                ledBuffer.setRGB(i, 0, 0, 0);
             } else {
+                led.start();
                 timer.restart();
             }
         }
         led.setData(ledBuffer);
+
     }
 }
