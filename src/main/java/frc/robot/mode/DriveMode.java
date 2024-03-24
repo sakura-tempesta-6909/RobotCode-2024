591f1f3d838fc4d4def8d26cdcdd5a6250fad80d
miyu <happysweets2026@gmail.com>
2024-03-24 18:13:44 +0900

    Modeの場所を変えた

--- src/main/java/frc/robot/mode/DriveMode.java
+++ src/main/java/frc/robot/mode/DriveMode.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.domain.model.DriveModel;
 import frc.robot.domain.model.ShooterModel;
 import frc.robot.domain.model.DriveModel.DriveOriented;
+import frc.robot.domain.model.LinkModel;
+import frc.robot.domain.model.LinkModel.ShooterAngleMode;
 import frc.robot.domain.model.ShooterModel.ShooterMode;
 
 class DriveMode extends ModeManager {
@@ -20,6 +22,61 @@ class DriveMode extends ModeManager {
             }
         } 
         
+
+        /**SHOOTER LINK系 */
+
+        /** SOURCEからのインテイクの角度にする＆インテイクする */
+        if(operateController.getRightBumper()) {
+            LinkModel.shooterAngleMode = ShooterAngleMode.s_intakeNote;
+            ShooterModel.shooterMode = ShooterMode.s_intake;
+        }
+        
+        /** アウトテイクする */
+        if(operateController.getXButton()) {
+            ShooterModel.shooterMode = ShooterMode.s_outtake;
+        }
+
+        /** SPEAKERの真下からのシュートの角度にする＆回転速度を上げる */
+        if(operateController.getYButton()) {
+            LinkModel.shooterAngleMode = ShooterAngleMode.s_speakerShootBelow;
+            ShooterModel.shooterMode = ShooterMode.s_increaseRotation;
+        }
+
+        /** podiumからのシュートの角度にする＆回転速度を上げる  */
+        if(operateController.getBButton()) {
+            LinkModel.shooterAngleMode = ShooterAngleMode.s_speakerShootPodium;
+            ShooterModel.shooterMode = ShooterMode.s_increaseRotation;
+        }
+
+        /** SPEAKERのleft&rightからのシュートの角度にする＆回転速度上げる */
+        if(operateController.getAButton()) {
+            LinkModel.shooterAngleMode = ShooterAngleMode.s_speakerShootSide;
+            ShooterModel.shooterMode = ShooterMode.s_increaseRotation;
+        }
+
+        /**  SPEAKERにシュートする*/
+        if(-1 <= operateController.getRightTriggerAxis() && operateController.getRightTriggerAxis() <= 1) {
+            ShooterModel.shooterMode = ShooterMode.s_shootSpeaker;
+        }
+
+        /** AMPからのシュートの角度にする＆回転速度を上げる */
+        if(operateController.getLeftBumper()) {
+            LinkModel.shooterAngleMode = ShooterAngleMode.s_speakerShootSide;
+            ShooterModel.shooterMode = ShooterMode.s_increaseRotation;
+        }
+
+        /** AMPにシュートする */
+        if(-1 <= operateController.getLeftTriggerAxis() && operateController.getLeftTriggerAxis() <= 1) {
+            ShooterModel.shooterMode = ShooterMode.s_shootAmp;
+        }
+
+
+        /** SPEAKERの左右からShootできるようにリンクの角度を変えて回転数を早くする */
+        if(operateController.getBButton()) {
+            LinkModel.shooterAngleMode = ShooterAngleMode.s_speakerShootSide;
+            ShooterModel.shooterMode = ShooterMode.s_increaseRotation;
+        }
+
     }
     
 }
--- src/main/java/frc/robot/mode/ShootMode.java
+++ src/main/java/frc/robot/mode/ShootMode.java
@@ -8,58 +8,7 @@ import frc.robot.domain.model.ShooterModel.ShooterMode;
 
 class ShootMode extends ModeManager {
     public static void changeModel() {
-        /** SOURCEからのインテイクの角度にする＆インテイクする */
-        if(operateController.getRightBumper()) {
-            LinkModel.shooterAngleMode = ShooterAngleMode.s_intakeNote;
-            ShooterModel.shooterMode = ShooterMode.s_intake;
-        }
         
-        /** アウトテイクする */
-        if(operateController.getXButton()) {
-            ShooterModel.shooterMode = ShooterMode.s_outtake;
-        }
-
-        /** SPEAKERの真下からのシュートの角度にする＆回転速度を上げる */
-        if(operateController.getYButton()) {
-            LinkModel.shooterAngleMode = ShooterAngleMode.s_speakerShootBelow;
-            ShooterModel.shooterMode = ShooterMode.s_increaseRotation;
-        }
-
-        /** podiumからのシュートの角度にする＆回転速度を上げる  */
-        if(operateController.getBButton()) {
-            LinkModel.shooterAngleMode = ShooterAngleMode.s_speakerShootPodium;
-            ShooterModel.shooterMode = ShooterMode.s_increaseRotation;
-        }
-
-        /** SPEAKERのleft&rightからのシュートの角度にする＆回転速度上げる */
-        if(operateController.getAButton()) {
-            LinkModel.shooterAngleMode = ShooterAngleMode.s_speakerShootSide;
-            ShooterModel.shooterMode = ShooterMode.s_increaseRotation;
-        }
-
-        /**  SPEAKERにシュートする*/
-        if(-1 <= operateController.getRightTriggerAxis() && operateController.getRightTriggerAxis() <= 1) {
-            ShooterModel.shooterMode = ShooterMode.s_shootSpeaker;
-        }
-
-        /** AMPからのシュートの角度にする＆回転速度を上げる */
-        if(operateController.getLeftBumper()) {
-            LinkModel.shooterAngleMode = ShooterAngleMode.s_speakerShootSide;
-            ShooterModel.shooterMode = ShooterMode.s_increaseRotation;
-        }
-
-        /** AMPにシュートする */
-        if(-1 <= operateController.getLeftTriggerAxis() && operateController.getLeftTriggerAxis() <= 1) {
-            ShooterModel.shooterMode = ShooterMode.s_shootAmp;
-        }
-
-
-        /** SPEAKERの左右からShootできるようにリンクの角度を変えて回転数を早くする */
-        if(operateController.getBButton()) {
-            LinkModel.shooterAngleMode = ShooterAngleMode.s_speakerShootSide;
-            ShooterModel.shooterMode = ShooterMode.s_increaseRotation;
-        }
-
 
     }
     
