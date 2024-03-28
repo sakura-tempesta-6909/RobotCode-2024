a8031156d1ae8c85f847e224f62514bf09a1b809
HatsuneHogosha <pi314159265358979@icloud.com>
2024-03-28 19:10:13 +0900

    periodicを呼び出しました

--- src/main/java/frc/robot/components/drive/infrastructure/Drive.java
+++ src/main/java/frc/robot/components/drive/infrastructure/Drive.java
@@ -52,5 +52,6 @@ public class Drive implements DriveRepository {
     @Override
     public void readSensors() {
         SmartDashboard.putNumber("Robot Heading", driveSubsystem.getHeading());
+        driveSubsystem.periodic();
     }
 }
