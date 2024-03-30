b7e781d657cfb1ebc29e35f01def2ce0f270c6cd
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2024-03-30 17:45:25 +0900

    overshoot直した

--- src/main/java/frc/robot/components/shooter/ShooterParameter.java
+++ src/main/java/frc/robot/components/shooter/ShooterParameter.java
@@ -11,9 +11,10 @@ public class ShooterParameter {
         public static final double Neutral = 0;
     }
     public static class PID {
-        public static final double ShooterP = 0.0002;
+        public static final double UpperShooterP = 0.0002;
         public static final double ShooterI = 5e-7;
         public static final double ShooterD = 0;
+        public static final double LowerShooterP = 0.0001;
     }
     public static void ConstInit() {
 
