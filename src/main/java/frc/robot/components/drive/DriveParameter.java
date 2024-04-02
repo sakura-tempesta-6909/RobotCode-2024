dabd67490a5962c0a61e91101fa1019a4f03df12
HatsuneHogosha <pi314159265358979@icloud.com>
2024-04-03 08:29:31 +0900

    driveparameterの説明を書きました

--- src/main/java/frc/robot/components/drive/DriveParameter.java
+++ src/main/java/frc/robot/components/drive/DriveParameter.java
@@ -3,18 +3,28 @@ package frc.robot.components.drive;
 public class DriveParameter {
     /** ロボットの速さを3段階で調整する */
     public static final class Speeds {
+        /** ロボットを止めるときの速度 */
         public static final double Neutral = 0;
 
+        /** ロボットが速いときのDriveMotorの速度 */
         public static final double FastDrive = 0.8;
+        /** ロボットが普通の速さのときのDriveMotorの速度 */
         public static final double MidDrive = 0.4;
+        /** ロボットが遅いときのDriveMotorの速度 */
         public static final double SlowDrive = 0.25;
 
+        /** ロボットが速いときのTurningMotorの速度 */
         public static final double FastThetaDrive = 0.6;
+        /** ロボットが普通の速さときのTurningMotorの速度 */
         public static final double MidThetaDrive = 0.3;
+        /** ロボットが遅いときのTurningMotorの速度 */
         public static final double SlowThetaDrive = 0.2;
 
+        /** gyroのPID制御　Pの値 */
         public static final double kP = 0.008;
+        /** gyroのPID制御　Iの値 */
         public static final double kI = 0.0005;
+        /** gyroのPID制御　Dの値 */
         public static final double kD = 0;
         
     }
